c2fe4ced7bccdce8ad2b734da0b233601a81c0a2
RaghavMaskara21 <raghavmaskara@gmail.com>
2021-08-04 12:04:50 +0530

    trial 4

--- BarnSizeCalculator/BarnSizeCalculator.js
+++ BarnSizeCalculator/BarnSizeCalculator.js
@@ -11,7 +11,7 @@ var fin = Number(final.value);
 
 var nou;
 var total=0;
-if(ini<1000 && fin<=1000 && ini%25==0 && fin%25==0) {
+if(ini<1000 && fin<=1000 && ini%25==0 && fin%25==0 && ini<fin) {
     nou= (fin-ini)/25;
     in_bem = (ini/25)-1;
     for ( var i=1; i<=nou;i++)
@@ -20,9 +20,12 @@ if(ini<1000 && fin<=1000 && ini%25==0 && fin%25==0) {
        in_bem +=1;
        }
     console.log(total);
-    document.getElementById("display").innerHTML = total + " Each Bems Needed For Upgrade";
+    document.getElementById("display1").innerHTML = total;
+    document.getElementById("display2").innerHTML = total;
+    document.getElementById("display3").innerHTML = total;
+    document.getElementById("displayNull").innerHTML = " ";
 }
-else if (ini<1000 && fin>1000 && ini%25==0 && fin%25==0) {
+else if (ini<1000 && fin>1000 && ini%25==0 && fin%25==0 && ini<fin) {
 nou= ((1000-ini)/25) + ((fin-1000)/50);
 in_bem = (ini/25)-1;
 for ( var i=1; i<=nou;i++)
@@ -31,9 +34,12 @@ total=total + in_bem;
 in_bem +=1;
 }
 console.log(total);
-document.getElementById("display").innerHTML = total + " Each Bems Needed For Upgrade";
+document.getElementById("display1").innerHTML = total;
+document.getElementById("display2").innerHTML = total;
+document.getElementById("display3").innerHTML = total;
+document.getElementById("displayNull").innerHTML = " ";
 }
-else if (ini>= 1000 && fin>1000 && ini%25==0 && fin%25==0) {
+else if (ini>= 1000 && fin>1000 && ini%25==0 && fin%25==0 && ini<fin) {
     nou= (fin-ini)/50;
 in_bem = 39 + ((ini-1000)/50);
 for ( var i=1; i<=nou;i++)
@@ -42,11 +48,18 @@ total=total + in_bem;
 in_bem +=1;
 }
 console.log(total);
-document.getElementById("display").innerHTML = total + " Each Bems Needed For Upgrade";
+document.getElementById("display1").innerHTML = total;
+document.getElementById("display2").innerHTML = total;
+document.getElementById("display3").innerHTML = total;
+document.getElementById("displayNull").innerHTML = " ";
 }
 else{
-    document.getElementById("display").innerHTML = "Enter A Valid Barn Size";
+    document.getElementById("display1").innerHTML = " ";
+    document.getElementById("display2").innerHTML = " ";
+    document.getElementById("display3").innerHTML = " ";
+    document.getElementById("displayNull").innerHTML = "Enter A Valid Barn Size";
 }
 
 
 }
+
